97b385503227f1680e2d35eb6a6514d3dd5da0fb
DESKTOP-NRV9GE8\jinyoungsoo <rokjinyoungsoo@gmail.com>
2023-06-26 13:27:44 +0900

    Jin:(ReadMe.md) Update ReadMe file.

---
+++ BoyStory_20230623/BoyStory_20230623/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoyStory_20230623
+{
+    public class Item
+    {
+
+        public void 
+
+    }
+}
